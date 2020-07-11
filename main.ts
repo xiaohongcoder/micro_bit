d9c706e5262e56b19bc5858f14d7169ca3acad43
xiaohong <61079864+xiaohongcoder@users.noreply.github.com>
2020-07-12 02:26:51 +0800

    延迟1小时测试准确

--- main.ts
+++ main.ts
@@ -1,48 +1,63 @@
 input.onButtonPressed(Button.A, function () {
-    basic.clearScreen()
+    basic.showLeds(`
+        . # # # .
+        # . . . #
+        # . . . #
+        # . . . #
+        . # # # .
+        `)
+    basic.pause(2000)
+    basic.showLeds(`
+        . . # . .
+        . # # # .
+        # # # # #
+        . # # # .
+        . # . # .
+        `)
+    basic.showLeds(`
+        . . . . .
+        . # # # .
+        # # # # #
+        . # # # .
+        . # . # .
+        `)
     basic.showLeds(`
         . . . . .
         . . . . .
-        . . . . .
-        . . . . .
         # # # # #
+        . # # # .
+        . # . # .
         `)
     basic.showLeds(`
         . . . . .
         . . . . .
         . . . . .
-        # # # # #
-        # # # # #
+        . # # # .
+        . # . # .
         `)
     basic.showLeds(`
         . . . . .
         . . . . .
-        # # # # #
-        # # # # #
-        # # # # #
+        . . . . .
+        . . . . .
+        . # . # .
         `)
     basic.showLeds(`
         . . . . .
-        # # # # #
-        # # # # #
-        # # # # #
-        # # # # #
+        . . . . .
+        . . . . .
+        . . . . .
+        . . . . .
         `)
-    basic.showLeds(`
-        # # # # #
-        # # # # #
-        # # # # #
-        # # # # #
-        # # # # #
-        `)
-    basic.pause(60000)
-    basic.showIcon(IconNames.Yes)
+    basic.pause(1000)
 })
-for (let index = 0; index < 3; index++) {
-    basic.showIcon(IconNames.SmallHeart)
-    basic.showIcon(IconNames.Heart)
-}
-basic.showIcon(IconNames.Heart)
+basic.showLeds(`
+    # # # # #
+    # # # # #
+    # # # # #
+    # # # # #
+    # # # # #
+    `)
 basic.forever(function () {
 	
 })
