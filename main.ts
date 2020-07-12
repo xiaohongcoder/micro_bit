22e2d9b069083b48f6256a8338e1d264ff85022c
xiaohong <61079864+xiaohongcoder@users.noreply.github.com>
2020-07-12 16:05:16 +0800

    每一小时点亮一行（从上到下）

--- main.ts
+++ main.ts
@@ -1,12 +1,70 @@
 input.onButtonPressed(Button.A, function () {
+    basic.clearScreen()
+    for (let index = 0; index < 3; index++) {
+        basic.showLeds(`
+            . . . . .
+            . # # # .
+            . # . # .
+            . # # # .
+            . . . . .
+            `)
+        basic.showLeds(`
+            # . # . #
+            . # # # .
+            # # . # #
+            . # # # .
+            # . # . #
+            `)
+    }
+    basic.clearScreen()
+    for (let index = 0; index < 1; index++) {
+        // 延迟 1 小时
+        basic.pause(3600000)
+    }
     basic.showLeds(`
-        . # # # .
-        # . . . #
-        # . . . #
-        # . . . #
-        . # # # .
+        . . . . .
+        . . . . .
+        . . . . .
+        . . . . .
+        . # . # .
         `)
-    basic.pause(2000)
+    for (let index = 0; index < 1; index++) {
+        // 延迟 1 小时
+        basic.pause(3600000)
+    }
+    basic.showLeds(`
+        . . . . .
+        . . . . .
+        . . . . .
+        . # # # .
+        . # . # .
+        `)
+    for (let index = 0; index < 1; index++) {
+        // 延迟 1 小时
+        basic.pause(3600000)
+    }
+    basic.showLeds(`
+        . . . . .
+        . . . . .
+        # # # # #
+        . # # # .
+        . # . # .
+        `)
+    for (let index = 0; index < 1; index++) {
+        // 延迟 1 小时
+        basic.pause(3600000)
+    }
+    basic.showLeds(`
+        . . . . .
+        . # # # .
+        # # # # #
+        . # # # .
+        . # . # .
+        `)
+    for (let index = 0; index < 1; index++) {
+        // 延迟 1 小时
+        basic.pause(3600000)
+    }
     basic.showLeds(`
         . . # . .
         . # # # .
@@ -14,43 +72,17 @@ input.onButtonPressed(Button.A, function () {
         . # # # .
         . # . # .
         `)
-    basic.showLeds(`
-        . . . . .
-        . # # # .
-        # # # # #
-        . # # # .
-        . # . # .
-        `)
-    basic.showLeds(`
-        . . . . .
-        . . . . .
-        # # # # #
-        . # # # .
-        . # . # .
-        `)
-    basic.showLeds(`
-        . . . . .
-        . . . . .
-        . . . . .
-        . # # # .
-        . # . # .
-        `)
-    basic.showLeds(`
-        . . . . .
-        . . . . .
-        . . . . .
-        . . . . .
-        . # . # .
-        `)
-    basic.showLeds(`
-        . . . . .
-        . . . . .
-        . . . . .
-        . . . . .
-        . . . . .
-        `)
-    basic.pause(1000)
+    basic.pause(5000)
+    basic.showIcon(IconNames.Yes)
 })
+function 增加1度 () {
+    servos.P0.setAngle(0)
+    basic.pause(1000)
+    servos.P0.setAngle(30)
+    basic.pause(1000)
+    servos.P0.setAngle(0)
+    basic.pause(1000)
+}
 basic.showLeds(`
     # # # # #
     # # # # #
